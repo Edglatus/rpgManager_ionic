3e9d000e523be695387cb157146abd5f71cb7558
Edglatus <tabrinath@gmail.com>
2020-01-22 01:08:40 -0300

    added custom expanding component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import { ExpandableComponent } from './expandable/expandable.component';
+
+@NgModule({
+  declarations: [ExpandableComponent],
+  exports: [ExpandableComponent]
+})
+
+export class ComponentsModule {}
